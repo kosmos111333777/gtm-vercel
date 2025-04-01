2931820b5067918e114602dfe64f998b50d0a02b
kosmos111333777 <105503447+kosmos111333777@users.noreply.github.com>
2025-04-01 21:51:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = (req, res) => res.status(200).send('GTM Server Ready');
+module.exports = (req, res) => {
+  if (req.url === '/gtm/healthy') {
+    res.status(200).send('Healthy ✔️');
+  } else {
+    res.status(404).send('Not Found');
+  }
+};
